8ba0d768b1c311ac11e46b5f2dfe61102a6321b4
sxuu0072 <sxuu0072@student.monash.edu>
2023-10-12 17:54:20 +1100

    add list-categories angular conponent

---
+++ frontend/src/app/components/silin/list-categories/list-categories.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
+
+
+@Component({
+  selector: 'app-list-categories',
+  templateUrl: './list-categories.component.html',
+  styleUrls: ['./list-categories.component.css']
+})
+export class ListCategoriesComponent {
+  records: any[] = [];
+
+  constructor(private dbService: DatabaseService) {
+    this.getRecords();
+  }
+
+  getRecords() {
+    this.dbService.getCategories().subscribe({
+      next: (data: any) => {
+        this.records = data;
+      },
+      error: (err:any) => { }
+    })
+  }
+
+}
